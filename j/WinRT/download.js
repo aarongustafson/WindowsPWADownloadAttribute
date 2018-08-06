39b486edf3d04970c95bf0aca53909cc9e948f7f
Aaron Gustafson <aaron@easy-designs.net>
2018-08-06 11:55:19 -0700

    Let’s try adding some WinRT to polyfill downloads

---
+++ j/WinRT/download.js
@@ -0,0 +1,59 @@
+(function(window, Windows){
+
+  // modern Edge engine only
+  if ( ! ('matches' in document.body ) ) return;
+  
+  document.body.addEventListener('click',function(e){
+    if (e.target.matches('a[download]'))
+    {
+      download(e.target.href);
+    }
+  }, false);
+
+  var downloader = new Windows.Networking.BackgroundTransfer.BackgroundDownloader();
+
+  function download(uri)
+  {
+    var filename = uri.split('/').pop(),
+        requestOperations = [],
+        download,
+        promise;
+    
+    if ( ! filename || filename == '' )
+    {
+      filename = 'download-' + (new Date).getTime();
+    }
+    
+    // Create the new file to download
+    Windows.Storage.DownloadsFolder.CreateFileAsync(filename, Windows.Storage.CreationCollisionOption.generateUniqueName)
+      .done(function(newFile){
+      
+        // set up the download
+        download = downloader.createDownload(uri, newFile);
+        download.priority = Windows.Networking.BackgroundTransfer.BackgroundTransferPriority.high;
+        requestOperations.push(download);
+
+        var requestPromise,
+            notImplementedException = -2147467263;
+
+        try {
+          requestPromise = Windows.Networking.BackgroundTransfer.BackgroundDownloader.requestUnconstrainedDownloadsAsync(requestOperations);
+        } catch (error) {
+          if (error.number === notImplementedException) {
+              displayError("BackgroundDownloader.requestUnconstrainedDownloadsAsync is not supported in Windows Phone.");
+              return;
+          }
+          throw error;
+        }
+
+        requestPromise.done(function(){
+          promise = download.startAsync().then(function(){
+            console.log('download complete');
+          });
+        });
+
+      });
+
+  }
+
+}(this, this.Windows));
---
+++ j/main.js
@@ -0,0 +1,17 @@
+/**
+ * PWA in Windows!
+ */
+if ( 'Windows' in window )
+{
+  var WinRT_js_files = [
+        '/j/WinRT/download.js'
+      ],
+      script = document.createElement('script');
+
+  WinRT_js_files.forEach(function(filename){
+    var js = script.cloneNode(true);
+    js.src = filename;
+    document.body.appendChild(js);
+  });
+
+}
